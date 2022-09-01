84542b3e1060ff1a7ecfd538d9bd68767807d7c4
Pramodkumar0 <109613194+Pramodkumar0@users.noreply.github.com>
2022-09-01 19:58:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+function callme(){
+    console.log("zero parameter")
+}
+callme()
+
+
+
+
+
+
+function text(a,b){
+    console.log(a+b)
+}
+text(3,4);
+
+
+
+
+var x = 21;
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl ();
+
+
+
+
+var x = 21;
+girl ();
+console.log(x)
+function girl() {
+    console.log(x);
+    var x = 20;
+};
+
+
+
+
+// var x = 21;
+// a();
+// b();
+// console.log(a);
+// a = function() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+// b = function() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+
+
+
+const test = (c,d) => {
+
+    console.log(c,d);
+}
+test(2,6);
+
+
+
+
+   
+    
+ function text(n) {
+    let a = 1;
+     for (let i=1; i<=n; i++){
+         a = a * i;
+     }
+     console.log(n);
+ }
+ text(8);
+
+
+
+
+
+
+
+
